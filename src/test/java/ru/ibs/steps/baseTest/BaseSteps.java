f55603bc6ce2b8b2cebaf6019c8f8a56e713c29d
kscclov <danteffy15@gmail.com>
2024-08-31 18:37:12 +0300

    Cucumber & Allure for Jenkins with selenoid type

--- src/test/java/ru/ibs/steps/baseTest/BaseSteps.java
+++ src/test/java/ru/ibs/steps/baseTest/BaseSteps.java
@@ -5,31 +5,49 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import java.net.*;
+
+import java.io.FileInputStream;
 import java.util.*;
+import java.util.Properties;
 
 public class BaseSteps {
-
+    private static WebDriver driver;
+    private static Properties props = new Properties();
     private static int idPreviousProduct;
-    public static WebDriver driver = setupRemoteDriver();
-
     @BeforeAll
-    public static RemoteWebDriver setupRemoteDriver() {
+    public static void setup() {
+        try (FileInputStream input = new FileInputStream("path/to/application.properties")) {
+            props.load(input);
+            String driverType = props.getProperty("type.driver");
+            String selenoidUrl = props.getProperty("selenoid.url");
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        Map<String, Object> selenoidOptions = new HashMap<>();
-        selenoidOptions.put("browserName", "chrome");
-        selenoidOptions.put("browserVersion", "109.0");
-        selenoidOptions.put("enableVNC", true);
-        selenoidOptions.put("enableVideo", false);
-        capabilities.setCapability("selenoid:options", selenoidOptions);
-        try{
-            return new RemoteWebDriver(URI.create("http://149.154.71.152:4444/wd/hub").toURL(), capabilities);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
+            if("remote".equalsIgnoreCase(driverType)) {
+                initRemoteDriver(selenoidUrl);
+            } else {
+
+                driver = new ChromeDriver();
+                System.setProperty("webdriver.chromedriver.driver",
+                        "\\src\\test\\resources\\chromedriver");
+
+                driver.manage().window().maximize();
+                //driver.get("http://localhost:8080/food");
+                driver.get("http://149.154.71.152:8080/food");
+            }
+        } catch (Exception e){
+
         }
+    }
+    public static void initRemoteDriver(String selenoidUrl){
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("109.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+    }
+    public static WebDriver getDriver() {
+        return driver;
     }
 
     public static int findPreviousProductId() {
@@ -39,17 +57,17 @@ public class BaseSteps {
                     "(//tr)[last()]/th")).getText());
             return idPreviousProduct;
         } catch (Exception e) {
-            Assertions.fail("Неправильное значение - невозможно преобразовать в int");
+            Assertions.fail("Invalid id value - cannot be converted to int.");
             return 0;
         }
     }
 
-    public static WebDriver getDriver() {
-        return driver;
+    public static int getIdPreviousProduct(){
+        return idPreviousProduct;
     }
 
     @AfterAll
-    public static void quitDriver() {
+    public static void endTesting() {
         driver.quit();
     }
 }
