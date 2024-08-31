85045eeb4f36e7843bf57401111e5d7e33ba8b41
kscclov <danteffy15@gmail.com>
2024-08-31 19:40:08 +0300

    Cucumber & Allure for Jenkins with selenoid type

--- src/test/java/ru/ibs/steps/baseTest/BaseSteps.java
+++ src/test/java/ru/ibs/steps/baseTest/BaseSteps.java
@@ -15,7 +15,7 @@ import java.net.URL;
 import java.util.*;
 
 public class BaseSteps {
-    public static final WebDriver driver;
+    public static WebDriver driver;
     private static Properties props = new Properties();
     private static int idPreviousProduct;
     @BeforeAll
